c9efae535ef36d8b23c6294f6b54eb2f73bed0ef
programacaomarilia <marilia.alves.vieira@gmail.com>
2026-06-25 19:54:28 +0000

    Inclusão de dados

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -6,11 +6,18 @@
 
 int main() {
   // Área para definição das variáveis para armazenar as propriedades das cidades
-  printf ("Ola mundo!, no Codespace\n");
-  printf ("Exemplo de edição de código!\n");
+  char estado = 'A';
+  char codigo_da_carta[] = "A01";
+  char cidade[] = "São Paulo";
+  int população = 12325000;
+  float area = 1521,11;
+  float pib = 699,28;
+  int numeros_de_pontos_turisticos = 50;
+
+printf("Estado: %s\n", estado);
   // Área para entrada de dados
 
   // Área para exibição dos dados da cidade
-
+  
 return 0;
 } 
